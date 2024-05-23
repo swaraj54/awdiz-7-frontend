bd4f6a5fb0d905576e3778ee20168db538bcf9e7
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-05-23 20:40:04 +0530

    Updates

--- day-0/src/App.js
+++ day-0/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <div>Hi</div>
+    </div>
+  );
 }
+
 
 export default App; // function
